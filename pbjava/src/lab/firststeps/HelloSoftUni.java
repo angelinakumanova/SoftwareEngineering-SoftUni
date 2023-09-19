0725d72ae4aaf93a916dc59f5ea976257c3dede6
Angelina Kumanova <angelinakumanova@gmail.com>
2023-09-20 01:40:32 +0300

    Changed directory

--- pbjava/src/lab/firststeps/HelloSoftUni.java
+++
@@ -1,7 +0,0 @@
-package lab.firststeps;
-
-public class HelloSoftUni {
-    public static void main(String[] args) {
-        System.out.println("Hello SoftUni");
-    }
-}
--- pbjava/src/whileLoopsExercises/StreamOfLetters.java
+++
@@ -1,7 +0,0 @@
-package whileLoopsExercises;
-
-public class StreamOfLetters {
-    public static void main(String[] args) {
-
-    }
-}

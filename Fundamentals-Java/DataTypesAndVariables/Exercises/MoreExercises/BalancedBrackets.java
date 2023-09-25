5cc0d8cc040541e6c2755af327654e1d7e7c3c55
Angelina Kumanova <angelinakumanova@gmail.com>
2023-09-25 22:01:33 +0300

    Added BalancedBrackets to MoreExercises

--- Fundamentals-Java/DataTypesAndVariables/Exercises/MoreExercises/BalancedBrackets.java
+++ Fundamentals-Java/DataTypesAndVariables/Exercises/MoreExercises/BalancedBrackets.java
@@ -6,9 +6,32 @@ public class BalancedBrackets {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int lines = Integer.parseInt(scanner.nextLine());
+        int countLeftBracket = 0;
+        int countRightBracket = 0;
 
-        for (int i = 0; i < lines; i++) {
+        for (int i = 1; i <= lines; i++) {
+            String input = scanner.nextLine();
+            if (i == 1 &&input.equals(")")) {
+                System.out.println("UNBALANCED");
+                return;
+            }
+            if (input.equals("(")) {
+                countLeftBracket++;
+            } else if (input.equals(")")) {
+                countRightBracket++;
+                if (countLeftBracket - countRightBracket != 0) {
+                    System.out.println("UNBALANCED");
+                    return;
+                }
+            }
+        }
+        if (countLeftBracket == countRightBracket) {
+            System.out.println("BALANCED");
+        } else {
+            System.out.println("UNBALANCED");
+        }
 
         }
-    }
+
 }
+

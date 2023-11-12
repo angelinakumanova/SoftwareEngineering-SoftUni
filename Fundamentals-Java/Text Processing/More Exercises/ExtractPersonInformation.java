7c50afb1ce252ea182081b409af14de74e20052c
Angelina Kumanova <angelinakumanova@gmail.com>
2023-11-12 18:42:31 +0200

    Added

---
+++ Fundamentals-Java/Text Processing/More Exercises/ExtractPersonInformation.java
@@ -0,0 +1,17 @@
+package TextProcessing.MoreExercises;
+
+import java.util.Scanner;
+
+public class ExtractPersonInformation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            String text = scanner.nextLine();
+            String name = text.substring(text.indexOf("@") + 1, text.indexOf("|"));
+            String age = text.substring(text.indexOf("#") + 1, text.indexOf("*"));
+            System.out.printf("%s is %s years old.%n", name, age);
+        }
+    }
+}

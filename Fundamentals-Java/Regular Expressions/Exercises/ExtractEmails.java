f6db1426c70442c89e8e1f135b959de16507176b
Angelina Kumanova <angelinakumanova@gmail.com>
2023-11-17 15:52:47 +0200

    Added

---
+++ Fundamentals-Java/Regular Expressions/Exercises/ExtractEmails.java
@@ -0,0 +1,18 @@
+package RegularExpressions.Exercises;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractEmails {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        Pattern pattern = Pattern.compile("(^|(?<=\\s))(([a-zA-Z0-9]+)([\\.\\-_]?)([A-Za-z0-9]+)(@)([a-zA-Z]+([\\.\\-_][A-Za-z]+)+))(\\b|(?=\\s))");
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group(0));
+        }
+    }
+}

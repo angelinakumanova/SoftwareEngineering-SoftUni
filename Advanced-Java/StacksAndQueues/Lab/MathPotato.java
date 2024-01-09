0b1dc79d1074ed2a0921a0b376172a2deedd017c
Angelina Kumanova <angelinakumanova@gmail.com>
2024-01-09 23:56:59 +0200

    Added

---
+++ Advanced-Java/StacksAndQueues/Lab/MathPotato.java
@@ -0,0 +1,47 @@
+package StacksAndQueues.Lab;
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class MathPotato {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] children = scanner.nextLine().split("\\s+");
+        int n = Integer.parseInt(scanner.nextLine());
+
+        PriorityQueue<String> queue = new PriorityQueue<>();
+        for (String child : children) {
+            queue.offer(child);
+        }
+
+        int cycle = 1;
+        while (queue.size() > 1) {
+            for (int i = 1; i < n; i++) {
+                queue.offer(queue.poll());
+            }
+
+            if (isPrime(cycle)) {
+                System.out.println("Prime " + queue.peek());
+            } else {
+                System.out.println("Removed " + queue.poll());
+            }
+
+            cycle++;
+        }
+
+        System.out.println("Last is " + queue.poll());
+
+    }
+
+    private static boolean isPrime(int n) {
+        if (n <= 1)
+            return false;
+
+        for (int i = 2; i < n; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    }
+}

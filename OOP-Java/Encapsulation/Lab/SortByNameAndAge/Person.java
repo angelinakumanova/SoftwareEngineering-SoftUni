b6528e55778ce613f7904fd1f5975abe3816bb68
Angelina Kumanova <angelinakumanova@gmail.com>
2024-02-26 23:39:09 +0200

    added

---
+++ OOP-Java/Encapsulation/Lab/SortByNameAndAge/Person.java
@@ -0,0 +1,43 @@
+package Encapsulation.Lab.SortByNameAndAge;
+
+public class Person {
+    private static final String STRING_TEMPLATE = "%s %s is %d years old.";
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setAge(age);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(STRING_TEMPLATE, this.firstName, this.lastName, this.age);
+    }
+
+    private void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    private void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    private void setAge(int age) {
+        this.age = age;
+    }
+}

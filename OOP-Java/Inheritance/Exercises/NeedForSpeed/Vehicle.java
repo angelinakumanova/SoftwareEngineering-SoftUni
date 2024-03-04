a5ee5a330adbad7529c8438008a0c278d12b16cc
Angelina Kumanova <angelinakumanova@gmail.com>
2024-03-04 03:09:29 +0200

    added

---
+++ OOP-Java/Inheritance/Exercises/NeedForSpeed/Vehicle.java
@@ -0,0 +1,51 @@
+package Inheritance.Exercises.NeedForSpeed;
+
+public class Vehicle {
+    private final static double DEFAULT_FUEL_CONSUMPTION = 1.25;
+    private double fuelConsumption;
+    private double fuel;
+    private int horsePower;
+
+    public Vehicle(double fuel, int horsePower) {
+        this.setFuel(fuel);
+        this.setHorsePower(horsePower);
+        this.setFuelConsumption(DEFAULT_FUEL_CONSUMPTION);
+
+    }
+
+    public void drive(double kilometers) {
+        double neededFuel = this.fuelConsumption * kilometers;
+
+        double remainingFuel = this.fuel - neededFuel;
+
+        if (remainingFuel >= 0) {
+            this.setFuel(remainingFuel);
+        }
+    }
+
+    public double getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public double getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(double fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+
+}

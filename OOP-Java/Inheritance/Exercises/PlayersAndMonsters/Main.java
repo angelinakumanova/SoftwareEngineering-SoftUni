6e232113eb45c6e8f67f8dac83e49e3f19e2f985
Angelina Kumanova <angelinakumanova@gmail.com>
2024-03-04 02:51:41 +0200

    added

---
+++ OOP-Java/Inheritance/Exercises/PlayersAndMonsters/Main.java
@@ -0,0 +1,7 @@
+package Inheritance.Exercises.PlayersAndMonsters;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ OOP-Java/Inheritance/Exercises/PlayersAndMonsters/Wizard.java
@@ -0,0 +1,7 @@
+package Inheritance.Exercises.PlayersAndMonsters;
+
+public class Wizard extends Hero{
+    public Wizard(String username, int level) {
+        super(username, level);
+    }
+}

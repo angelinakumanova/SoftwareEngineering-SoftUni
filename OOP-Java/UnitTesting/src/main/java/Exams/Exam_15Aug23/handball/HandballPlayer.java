eab49db29b54cd4b1dc75a5a8a08e1b9443b12bc
Angelina Kumanova <angelinakumanova@gmail.com>
2024-03-20 12:01:17 +0200

    added main

---
+++ OOP-Java/UnitTesting/src/main/java/Exams/Exam_15Aug23/handball/HandballPlayer.java
@@ -0,0 +1,27 @@
+package Exams.Exam_15Aug23.handball;
+
+public class HandballPlayer {
+    private String name;
+    private boolean isActive;
+
+    public HandballPlayer(String name){
+        this.setName(name);
+        this.setActive(true);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        this.isActive = active;
+    }
+}
---
+++ OOP-Java/UnitTesting/src/main/java/Exams/Exam_15Aug23/handball/Main.java
@@ -0,0 +1,7 @@
+package Exams.Exam_15Aug23.handball;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}

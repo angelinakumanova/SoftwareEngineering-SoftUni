0a3a5b251e3027031b1533216aa72420455ce93e
Angelina Kumanova <angelinakumanova@gmail.com>
2024-06-25 01:18:14 +0300

    added problem 10

---
+++ JS Front-End/SyntaxFundamentals_Lab/numbersFromMToN.js
@@ -0,0 +1,9 @@
+function printNumbers (firstNumber, secondNumber) {
+    for(let i = firstNumber; i >= secondNumber; i--) {
+        console.log(i);
+    }
+}
+
+printNumbers(6, 2);
+console.log('');
+printNumbers(4, 1);

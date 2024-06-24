84a9b2cb1bc2b1a71ab6b38b001a7e6a465bc0c5
Angelina Kumanova <angelinakumanova@gmail.com>
2024-06-25 00:33:33 +0300

    added problem 2

---
+++ JS Front-End/SyntaxFundamentals_Lab/studentInformation.js
@@ -0,0 +1,7 @@
+function printInfo(name, age, grade) {
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`)
+}
+
+printInfo('John', 15, 5.54678);
+printInfo('Steve', 16, 2.14);
+printInfo('Marry', 12, 6.00);

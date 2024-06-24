99c4a9fa89bc1e1dd9f8cdf51a3db94771741729
Angelina Kumanova <angelinakumanova@gmail.com>
2024-06-25 00:52:31 +0300

    added problem 6

---
+++ JS Front-End/SyntaxFundamentals_Lab/largestNumber.js
@@ -0,0 +1,8 @@
+function printLargestNumber(firstNumber, secondNumber, thirdNumber) {
+    let largestNumber = Math.max(firstNumber, secondNumber, thirdNumber);
+
+    console.log(`The largest number is ${largestNumber}.`)
+}
+
+printLargestNumber(5, -3, 16);
+printLargestNumber(-3, -5, -22.5);

8a1abb1d31730b5be0619773391e04fe819c9118
Angelina Kumanova <angelinakumanova@gmail.com>
2024-06-25 01:06:05 +0300

    added problem 7

---
+++ JS Front-End/SyntaxFundamentals_Lab/theatrePromotions.js
@@ -0,0 +1,41 @@
+function calculateTicket(day, age) {
+    switch(day) {
+        case 'Weekday':
+            if ((age >= 0 && age <= 18) || (age > 64 && age <= 122)) {
+                price = 12;
+            } else if (age > 18 && age <= 64) {
+                price = 18;
+            } else {
+                price = 'Error!';
+            }
+        break;
+        
+        case 'Weekend':
+            if ((age >= 0 && age <= 18) || (age > 64 && age <= 122)) {
+                price = 15;
+            } else if (age > 18 && age <= 64) {
+                price = 20;
+            } else {
+                price = 'Error!';
+            }
+        break;
+
+        case 'Holiday':
+            if (age >= 0 && age <= 18) {
+                price = 5;
+            } else if (age > 18 && age <= 64) {
+                price = 12;
+            } else if (age > 64 && age <= 122) {
+                price = 10;
+            } else {
+                price = 'Error!';
+            }
+        break;
+    }
+
+    console.log(price === 'Error!' ? price : price + '$');
+}
+
+calculateTicket('Weekday', 42);
+calculateTicket('Holiday', -12);
+calculateTicket('Holiday', 15);

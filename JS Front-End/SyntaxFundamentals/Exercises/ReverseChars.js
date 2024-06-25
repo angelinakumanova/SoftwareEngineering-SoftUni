abedc07335567fbd3f306260a3aa9e665c192444
Angelina Kumanova <angelinakumanova@gmail.com>
2024-06-25 21:29:54 +0300

    added problem 8

---
+++ JS Front-End/SyntaxFundamentals/Exercises/ReverseChars.js
@@ -0,0 +1,8 @@
+function reverseChars(firstChar, secondChar, thirdChar) {
+    let reversedChars = thirdChar + ' ' + secondChar + ' ' + firstChar;
+
+    console.log(reversedChars);
+}
+
+reverseChars('A', 'B', 'C');
+reverseChars('1', 'L', '&');

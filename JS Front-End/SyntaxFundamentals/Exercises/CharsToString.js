6789e159f8033492750d6a1f259e13552c971c34
Angelina Kumanova <angelinakumanova@gmail.com>
2024-06-25 21:26:18 +0300

    added problem 7

---
+++ JS Front-End/SyntaxFundamentals/Exercises/CharsToString.js
@@ -0,0 +1,9 @@
+function convertToString(firstChar, secondChar, thirdChar) {
+    let string = firstChar + secondChar + thirdChar;
+
+    console.log(string);
+}
+
+convertToString('a', 'b', 'c');
+convertToString('%', '2', 'o');
+convertToString('1', '5', 'p');

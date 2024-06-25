8fae1bca0d4a70bbaf6356076a4bb7490bac607f
Angelina Kumanova <angelinakumanova@gmail.com>
2024-06-25 14:49:58 +0300

    added problem 5

---
+++ JS Front-End/SyntaxFundamentals/Exercises/MultiplicationTable.js
@@ -0,0 +1,9 @@
+function printMultiplication(number) {
+    for(let i = 1; i <= 10; i++) {
+        console.log(`${number} X ${i} = ${number * i}`);
+    }
+}
+
+printMultiplication(5);
+console.log(' ');
+printMultiplication(2);

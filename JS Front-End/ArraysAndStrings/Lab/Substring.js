d2b30d5471398e2dc01d49bd63b172eb55a6e7bc
Angelina Kumanova <angelinakumanova@gmail.com>
2024-07-07 14:25:57 +0300

    added problem 4

---
+++ JS Front-End/ArraysAndStrings/Lab/Substring.js
@@ -0,0 +1,6 @@
+function solve(text, startingIndex, countOfElements) {
+    console.log(text.substring(startingIndex, startingIndex + countOfElements));
+}
+
+solve('ASentence', 1, 8);
+solve('SkipWord', 4, 7);

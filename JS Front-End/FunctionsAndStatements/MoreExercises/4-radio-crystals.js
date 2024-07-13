2c7d0c43922c73d7fc33b00393ff5497c1408dee
Angelina Kumanova <angelinakumanova@gmail.com>
2024-07-13 16:41:49 +0300

    added problem 4 solved fully

--- JS Front-End/FunctionsAndStatements/MoreExercises/4-radio-crystals.js
+++ JS Front-End/FunctionsAndStatements/MoreExercises/4-radio-crystals.js
@@ -1,12 +1,12 @@
 function solve(oreInfo) {
     const desiredThickness = oreInfo[0];
-
+    
     for(let i = 1; i < oreInfo.length; i++) {
         console.log(`Processing chunk ${oreInfo[i]} microns`);
         operate(oreInfo[i], desiredThickness);
     }
-
-
+    
+    
     function operate(oreThickness, desiredThickness) {
         
         const operations = new Map([
@@ -15,39 +15,43 @@ function solve(oreInfo) {
             ['Grind', a => a - 20],
             ['Etch', a => a - 2],
         ]);
-
+        
         for (let [key, value] of operations) {
             let count = 0;
-            while (value(oreThickness) >= desiredThickness - 1) {
+            const limit = key === 'Etch' ? desiredThickness - 1 : desiredThickness;
+
+            while (value(oreThickness) >= limit) {
                 oreThickness = value(oreThickness);
                 count++;
             }
-
-            console.log(`${key} x${count}`);
-            oreThickness = transport(oreThickness);
-
+            
+            if (count > 0) {
+                console.log(`${key} x${count}`);
+                oreThickness = transport(oreThickness);
+            }
+            
+            
+            
             if (oreThickness === desiredThickness) {
-                console.log(`Finished crystal ${desiredThickness} microns`);
                 break;
             }
+            
         }
-
-        doXrayOnCrystal(oreThickness, desiredThickness);
+        
+        if (oreThickness < desiredThickness) {
+            oreThickness = oreThickness + 1;
+            console.log('X-ray x1');
+        }
+        
+        console.log(`Finished crystal ${desiredThickness} microns`);
     }
-
+    
     function transport(oreThickness) {
         console.log('Transporting and washing');
-
+        
         return Math.floor(oreThickness);
     }
 
-    function doXrayOnCrystal(oreThickness, desiredThickness) {
-        if (oreThickness + 1 === desiredThickness) {
-            console.log('X-ray x1');
-            console.log(`Finished crystal ${desiredThickness} microns`);
-        }
-    }
 }
 
-solve([1375, 50000]);
-// solve([1000, 4000, 8100]);
+solve([100, 396]);

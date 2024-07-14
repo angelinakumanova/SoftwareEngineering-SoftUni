6b058c103c9adb33e472ef360ccd427980ff2f18
Angelina Kumanova <angelinakumanova@gmail.com>
2024-07-14 15:29:38 +0300

    refactored problem 2

--- JS Front-End/ObjectsAndClasses/Lab/2-city.js
+++ JS Front-End/ObjectsAndClasses/Lab/2-city.js
@@ -1,9 +1,9 @@
 function printObjectInfo(object) {
-    console.log(`name -> ${object.name}`);
-    console.log(`area -> ${object.area}`);
-    console.log(`population -> ${object.population}`);
-    console.log(`country -> ${object.country}`);
-    console.log(`postCode -> ${object.postCode}`);
+    const entries = Object.entries(object);
+
+    for (const [key, value] of entries) {
+        console.log(`${key} -> ${value}`)
+    }
 }
 
 printObjectInfo({

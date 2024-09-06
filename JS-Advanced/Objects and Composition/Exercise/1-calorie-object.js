dd2fb1b7377212e94a7ef813b3c6d4b29fb3a55a
Angelina Kumanova <angelinakumanova@gmail.com>
2024-09-06 11:04:27 +0300

    added solution to problem 1

---
+++ JS-Advanced/Objects and Composition/Exercise/1-calorie-object.js
@@ -0,0 +1,11 @@
+function solve(input) {
+    const products = {};
+
+    for (let i = 0; i < input.length; i += 2) {
+        products[input[i]] = Number(input[i + 1]);
+    }
+
+    console.log(products);
+}
+
+solve(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']);

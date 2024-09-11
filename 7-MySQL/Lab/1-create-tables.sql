ba5aafc193dac44f81c6740ed2c25fb222c5d813
Angelina Kumanova <angelinakumanova@gmail.com>
2024-09-11 14:59:56 +0300

    Create 1-create-tables.sql

---
+++ 7-MySQL/Lab/1-create-tables.sql
@@ -0,0 +1,21 @@
+USE gamebar;
+
+CREATE TABLE employees (
+id INT NOT NULL PRIMARY KEY,
+first_name VARCHAR(50) NOT NULL,
+last_name VARCHAR(50) NOT NULL
+);
+
+ALTER TABLE employees
+MODIFY id INT AUTO_INCREMENT;
+
+CREATE TABLE categories (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE products (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    category_id INT NOT NULL
+);

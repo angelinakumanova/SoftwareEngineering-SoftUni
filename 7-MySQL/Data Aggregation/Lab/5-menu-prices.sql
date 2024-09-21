8c618977884b72c9f56119a3260900188c26aabb
Angelina Kumanova <angelinakumanova@gmail.com>
2024-09-21 17:05:53 +0300

    added solution to problem 5

---
+++ 7-MySQL/Data Aggregation/Lab/5-menu-prices.sql
@@ -0,0 +1,7 @@
+SELECT 
+	category_id, 
+    ROUND(AVG(price), 2) AS 'Average Price',
+    MIN(price) AS 'Cheapest Product',
+    MAX(price) AS 'Most Expensive Product'
+FROM products
+GROUP BY category_id;

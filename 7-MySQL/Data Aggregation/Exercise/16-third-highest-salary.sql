4841aaa16fafebf23f73fcaa0be9338dfbf31f29
Angelina Kumanova <angelinakumanova@gmail.com>
2024-09-21 22:30:56 +0300

    added solution to problem 16

---
+++ 7-MySQL/Data Aggregation/Exercise/16-third-highest-salary.sql
@@ -0,0 +1,13 @@
+SELECT 
+	department_id, 
+	(SELECT DISTINCT salary
+	FROM employees
+	WHERE department_id = e.department_id
+	ORDER BY salary DESC
+	LIMIT 1 OFFSET 2) AS third_highest_salary
+FROM employees AS e
+GROUP BY department_id
+HAVING third_highest_salary IS NOT NULL
+ORDER BY department_id;
+
+

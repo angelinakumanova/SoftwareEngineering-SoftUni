bb44b67c35ae0bc07ff82990f21f656a7353af21
Angelina Kumanova <angelinakumanova@gmail.com>
2024-09-22 15:49:09 +0300

    added solution to problem 2

---
+++ JS-Advanced/Classes/Lab/2-get-persons.js
@@ -0,0 +1,25 @@
+class Person {
+    constructor(firstName, lastName, age, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+    }
+
+    toString() {
+        return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
+    }
+}
+
+function createPeople() {
+    const p1 = new Person('Anna', 'Simpson', 22, 'anna@yahoo.com');
+    const p2 = new Person('Softuni');
+    const p3 = new Person('Stephan', 'Johnson', 25);
+    const p4 = new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com');
+
+    return [p1, p2, p3, p4];
+}
+
+console.log(createPeople());
+
+

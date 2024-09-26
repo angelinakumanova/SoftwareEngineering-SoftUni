128882ecb638bf7aeeb7f0c772400b3e23272d74
Angelina Kumanova <angelinakumanova@gmail.com>
2024-09-26 15:40:12 +0300

    added solution to problem 5

---
+++ 7-MySQL/Table Relations/Lab/5-project-management-db.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE project_management;
+USE project_management;
+
+CREATE TABLE clients (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    client_name VARCHAR(100)
+);
+
+CREATE TABLE projects (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    client_id INT,
+    project_lead_id INT,
+    
+    FOREIGN KEY (client_id)
+    REFERENCES clients(id)
+);
+
+CREATE TABLE employees (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    project_id INT,
+    
+    FOREIGN KEY (project_id)
+    REFERENCES projects(id)
+);
+
+ALTER TABLE projects
+ADD FOREIGN KEY (project_lead_id)
+REFERENCES employees(id);

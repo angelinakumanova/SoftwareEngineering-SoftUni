69c3e8ce225bffb420096a2652f432df1afa1826
Angelina Kumanova <angelinakumanova@gmail.com>
2024-09-29 15:47:55 +0300

    added solution to problem 15

---
+++ 7-MySQL/Subqueries and JOINs/Exercise/15-continents-and-currencies.sql
@@ -0,0 +1,12 @@
+SELECT c.continent_code, c.currency_code, COUNT(*) as currency_usage
+FROM countries c
+GROUP BY c.continent_code, c.currency_code
+HAVING currency_usage > 1 AND currency_usage = (
+			SELECT COUNT(*) as currency_usage
+			FROM countries
+			WHERE continent_code = c.continent_code
+			GROUP BY currency_code
+			ORDER BY currency_usage DESC
+			LIMIT 1)
+ORDER BY c.continent_code, c.currency_code;
+

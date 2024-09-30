1a2cdc4b48caac684edad2c89a46d79f783b7eec
Angelina Kumanova <angelinakumanova@gmail.com>
2024-09-30 11:02:12 +0300

    added solution to problem 2

---
+++ 7-MySQL/_Exams/Exam_6August2023/02-insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO property_transactions
+(property_id, buyer_id, transaction_date, bank_name, iban, is_successful)
+SELECT 
+	agent_id + DAY(offer_datetime) AS property_id,
+    agent_id + MONTH(offer_datetime) AS buyer_id,
+    DATE(offer_datetime) AS transaction_date,
+    CONCAT('Bank ', agent_id) AS bank_name,
+    CONCAT('BG', price, agent_id) AS iban,
+    TRUE AS is_successful
+FROM property_offers
+WHERE agent_id <= 2;

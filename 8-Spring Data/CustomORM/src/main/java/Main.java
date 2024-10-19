61fb7c65671908d2f9bca659005e4a1df40870b4
Angelina Kumanova <angelinakumanova@gmail.com>
2024-10-19 14:05:21 +0300

    Update Main.java

--- 8-Spring Data/CustomORM/src/main/java/Main.java
+++ 8-Spring Data/CustomORM/src/main/java/Main.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        MyConnector.createConnection("root", "", "soft_uni");
+        MyConnector.createConnection("root", "123456aK", "soft_uni");
 
 
         Connection connection = MyConnector.getConnection();

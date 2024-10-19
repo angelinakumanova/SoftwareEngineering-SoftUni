8bf4b46b5417d767f04592ddb57c8c123c896783
Angelina Kumanova <angelinakumanova@gmail.com>
2024-10-19 14:09:03 +0300

    Added DbContext interface

--- 8-Spring Data/CustomORM/src/main/java/Main.java
+++ 8-Spring Data/CustomORM/src/main/java/Main.java
@@ -7,7 +7,6 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         MyConnector.createConnection("root", "123456aK", "soft_uni");
 
-
         Connection connection = MyConnector.getConnection();
 
     }

6ae65fde84b97b5a5abd7d2c7b805fe30eddc649
Angelina Kumanova <angelinakumanova@gmail.com>
2024-10-23 13:54:57 +0300

    added student entity and xml files

--- 8-Spring Data/IntroductionToHibernate/src/main/java/Hibernate/Main.java
+++ 8-Spring Data/IntroductionToHibernate/src/main/java/Hibernate/Main.java
@@ -1,7 +1,26 @@
 package Hibernate;
 
+import Hibernate.entities.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        Configuration cfg = new Configuration();
+        cfg.configure();
+
+        SessionFactory sf = cfg.buildSessionFactory();
+        Session session = sf.openSession();
+        session.beginTransaction();
+
+        Student student = new Student();
+        student.setName("Jack");
+        student.setAge(22);
+        session.persist(student);
+
+        session.getTransaction().commit();
+        session.close();
     }
 }

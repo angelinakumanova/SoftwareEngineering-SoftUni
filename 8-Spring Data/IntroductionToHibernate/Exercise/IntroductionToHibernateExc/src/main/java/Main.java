04874ab01154fea84e049e1d3659df828eee5f6f
Angelina Kumanova <angelinakumanova@gmail.com>
2024-10-24 16:26:10 +0300

    added solution to first problem

--- 8-Spring Data/IntroductionToHibernate/Exercise/IntroductionToHibernateExc/src/main/java/Main.java
+++ 8-Spring Data/IntroductionToHibernate/Exercise/IntroductionToHibernateExc/src/main/java/Main.java
@@ -1,15 +1,25 @@
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
+    private static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("soft_uni");
         EntityManager entityManager = emf.createEntityManager();
 
         entityManager.getTransaction().begin();
-        
 
+//        changeCasing(entityManager);
+
+        entityManager.getTransaction().commit();
         entityManager.close();
+    }
+
+    private static void changeCasing(EntityManager entityManager) {
+        entityManager.createQuery("UPDATE Town SET name = UPPER(name) WHERE LENGTH(name) > 5").executeUpdate();
     }
 }

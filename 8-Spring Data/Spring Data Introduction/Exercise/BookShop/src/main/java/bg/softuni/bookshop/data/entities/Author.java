6a2bdab45c528f5ef6b10aaace68df2ba204613c
Angelina Kumanova <angelinakumanova@gmail.com>
2024-11-02 00:08:18 +0200

    added entities and enums

---
+++ 8-Spring Data/Spring Data Introduction/Exercise/BookShop/src/main/java/bg/softuni/bookshop/data/entities/Author.java
@@ -0,0 +1,54 @@
+package bg.softuni.bookshop.data.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table (name = "authors")
+public class Author extends BaseEntity {
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @OneToMany(mappedBy = "author")
+    private Set<Book> books;
+
+
+    public Author() {
+        this.books = new HashSet<>();
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return Collections.unmodifiableSet(books);
+    }
+
+    public void addBook(Book book) {
+        if (book != null) {
+            this.books.add(book);
+        }
+    }
+}
---
+++ 8-Spring Data/Spring Data Introduction/Exercise/BookShop/src/main/java/bg/softuni/bookshop/data/entities/Category.java
@@ -0,0 +1,23 @@
+package bg.softuni.bookshop.data.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "categories")
+public class Category extends BaseEntity {
+    @Column(nullable = false)
+    private String name;
+
+    public Category() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

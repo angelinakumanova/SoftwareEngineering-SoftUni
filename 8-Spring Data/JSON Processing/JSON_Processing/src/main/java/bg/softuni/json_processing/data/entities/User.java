203918efba8518320a87bc654e109a55551e20d3
Andjelina Kumanova <angelinakumanova@gmail.com>
2024-11-10 20:35:57 +0200

    added project

---
+++ 8-Spring Data/JSON Processing/JSON_Processing/src/main/java/bg/softuni/json_processing/data/entities/User.java
@@ -0,0 +1,10 @@
+package bg.softuni.json_processing.data.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+
+}
---
+++ 8-Spring Data/JSON Processing/JSON_Processing/src/test/java/bg/softuni/json_processing/JsonProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.json_processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

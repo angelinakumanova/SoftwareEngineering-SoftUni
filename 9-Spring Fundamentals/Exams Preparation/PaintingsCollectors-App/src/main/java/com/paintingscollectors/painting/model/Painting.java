a372eaf0bc5b9b47fc07dcd07c9fc2e5f4f9a092
Andjelina Kumanova <angelinakumanova@gmail.com>
2025-02-12 11:59:06 +0200

    Add entities

---
+++ 9-Spring Fundamentals/Exams Preparation/PaintingsCollectors-App/src/main/java/com/paintingscollectors/painting/model/Painting.java
@@ -0,0 +1,43 @@
+package com.paintingscollectors.painting.model;
+
+import com.paintingscollectors.user.model.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Painting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Style style;
+
+    @ManyToOne
+    private User owner;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private int votes;
+
+
+}

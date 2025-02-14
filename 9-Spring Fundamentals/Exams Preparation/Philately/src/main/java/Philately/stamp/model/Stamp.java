c7c0802b77f8378ac10e3aebcd32edb6624dbd54
Andjelina Kumanova <angelinakumanova@gmail.com>
2025-02-14 14:42:53 +0200

    Add entities

---
+++ 9-Spring Fundamentals/Exams Preparation/Philately/src/main/java/Philately/stamp/model/Stamp.java
@@ -0,0 +1,37 @@
+package Philately.stamp.model;
+
+import Philately.user.model.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Stamp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Paper paper;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @ManyToOne
+    private User owner;
+}
---
+++ 9-Spring Fundamentals/Exams Preparation/Philately/src/main/java/Philately/user/model/User.java
@@ -0,0 +1,36 @@
+package Philately.user.model;
+
+import Philately.stamp.model.Stamp;
+import Philately.stamp.model.WishedStamp;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @OneToMany(mappedBy = "owner", fetch = FetchType.EAGER)
+    private List<Stamp> stamps;
+
+    @OneToMany(mappedBy = "owner", fetch = FetchType.EAGER)
+    private List<WishedStamp> wishedStamps;
+}
